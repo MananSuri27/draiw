4e130d15c171a4a64233df42f5de066048f95c71
Manan Suri <84636031+MananSuri27@users.noreply.github.com>
2022-12-02 22:09:53 +0530

    uhmm

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
     console.log(prompt);
 
     let res = await fetch(
-      "http://cors-anywhere.herokuapp.com/http://ec2-13-234-120-211.ap-south-1.compute.amazonaws.com:5000/hello",
+      "https://cors-anywhere.herokuapp.com/http://ec2-13-234-120-211.ap-south-1.compute.amazonaws.com:5000/hello",
       {
         headers: {
           Accept: "application/json",
